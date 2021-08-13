eba7fc9583187d24bb905cb141dc5fde881a1d71
MuhammadHusayn Olimjonov <olimjonov926@gmail.com>
2021-08-13 11:12:36 +0500

    client finished

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,12 @@
-console.log('hello')
+let backendHost = 'http://localhost:4500'
+
+;( async  () => {
+	let response = await fetch(backendHost + '/api/data')
+	let users  = await response.json()
+	for (let user of users) {
+		let li = document.createElement('li')
+		li.textContent = user.username
+
+		list.append(li)
+	}
+})()
